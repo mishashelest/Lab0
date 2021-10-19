cf48843deb85cebb2845f9d1135ebf7f40b64d88
mishashelest <91960577+mishashelest@users.noreply.github.com>
2021-10-19 22:00:30 +0300

    first commit

--- src/Lab/Lab_0_Variant26.java
+++ src/Lab/Lab_0_Variant26.java
@@ -1,40 +1,14 @@
 package Lab;
 import java.util.Scanner;
-
 public class Lab_0_Variant26 {
 
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-//        int N , M;
-//        M = in.nextInt(); // rows
-//        N = in.nextInt(); // cols
-//        int[][] array1 = { { 1, 1, 2 ,4}, { 3, 4, 5 ,1} };
-//        for (int i = 0; i < M; i++)
-//        {
-//            array1[i] = new int[N];
-//            for(int j = 0; j < N; j++)
-//            {
-//                array1[i][j] = in.nextInt();
-//            }
-//        }
+        int[] array = new int[]{2,4,1,2,2,2,2};
+        System.out.println(minn_max(7,array));
 
-//        matrix_array(array1,N,M);
-        System.out.println(int_ger(20));
-        int[] array = new int[]{2,1,2,2};
-        System.out.println(minn_max(4,array));
-//        for (int i = 0; i < M; i++)
-//        {
-//            for(int j = 0; j < N; j++)
-//            {
-//                System.out.print(array1[i][j] + "   ");
-//            }
-//            System.out.print("\n");
-//        }
-//        int[] array = new int[]{2,1,2,2};
-//        array_1_dimensional(array);
-//        System.out.println(array_1_dimensional(array));
-//        System.out.println( minn_max(2623));
+
 
     }
     public static int int_ger(int a)
@@ -93,8 +67,10 @@ public class Lab_0_Variant26 {
         return  sum;
     }
 
-    public  static  void while_loop(int N)
+    public  static  int[] while_loop(int N)
     {
+        int[] arr = new int [ 2];
+
         int last, next;
         int f = 1, g = 1;
         while(f < N )
@@ -102,40 +78,30 @@ public class Lab_0_Variant26 {
             f = f + g;
             g = f - g;
         }
-        last = g;
-        next = f + g;
-        System.out.println(last + "    " + next);
+        arr[0] = g;
+        arr[1] = f + g;
+        return arr;
     }
 
     public  static  int minn_max(int n , int[] array)
     {
-        int count1 = 0, count2 = 0;
-        boolean check = false;
+        int count = 0, sum = 0 ;
         for (int i = 0 ; i < n; i++ )
         {
-            if ( array[i] % 2 == 0 )
+            int j = i;
+            while( j < n && array[j] % 2 == 0 )
             {
-                check = true;
-                while (check)
-                {
-                    count2++;
-                }
-                check = false;
-                if(count1 < count2)
-                {
-                    count1 = count2;
-                }
-                count2 = 0;
+                count++;
+                j++;
             }
+            if(sum < count)
+            {
+                sum = count;
+            }
+            count = 0;
+
         }
-        if(count1 != 0)
-        {
-            return count1 ;
-        }
-        else
-        {
-            return 0;
-        }
+        return sum;
     }
 
     public static int array_1_dimensional(int[] array)
@@ -150,8 +116,9 @@ public class Lab_0_Variant26 {
         return 0;
     }
 
-    public  static void matrix_array(int[][] array,int N,int M)
+    public  static int[] matrix_array(int[][] array,int N,int M)
     {
+        int[] arr = new int[2];
         int min = 9999999, number = 0, sum = 1;
         for (int i = 0; i < N; i++)
         {
@@ -166,7 +133,9 @@ public class Lab_0_Variant26 {
             }
             sum = 1;
         }
-        System.out.println(min  + "   " + number);
+        arr[0] = min;
+        arr[1] = number;
+        return  arr ;
+
     }
 }
-///sdfsdsddssds
--- src/Test/Test_Lab_0_Variant26.java
+++ src/Test/Test_Lab_0_Variant26.java
@@ -55,15 +55,70 @@ public class Test_Lab_0_Variant26
     }
 
     @Test(dataProvider = "forProvider")
-    public void forTest(int number , float length, float check)
+    public void forTest(float x , int N, float check)
     {
-        assertEquals(new Lab_0_Variant26().case_stat(number,length) , check);
+        assertEquals(new Lab_0_Variant26().for_loop(x ,N), check);
     }
 
     @DataProvider
     public Object[][] forProvider()
     {
-        return new Object[][] {{2,2,2000},{1,300,30}};
+        return new Object[][] {{2,2,19.2F}};
+    }
+
+    @Test(dataProvider = "WhileProvider")
+    public void WhileTest(int N, int[] check )
+    {
+        assertEquals(new Lab_0_Variant26().while_loop(N), check);
+    }
+
+    @DataProvider
+    public Object[][] WhileProvider()
+    {
+        return new Object[][] {{13,new int[]{8,21}}};
+    }
+
+    @Test(dataProvider = "MinProvider")
+    public void MinTest(int n ,int[] array, float check)
+    {
+        assertEquals(new Lab_0_Variant26().minn_max(n,array), check);
+    }
+
+    @DataProvider
+    public Object[][] MinProvider()
+    {
+        return new Object[][] {{ 5 , new int[]{2,2,2,1,2} , 3}};
+    }
+
+    @Test(dataProvider = "ArrayProvider")
+    public void ArrayTest(int[] array, float check)
+    {
+        assertEquals(new Lab_0_Variant26().array_1_dimensional(array), check);
+    }
+
+    @DataProvider
+    public Object[][] ArrayProvider()
+    {
+        return new Object[][] {{ new int[]{2,1,2,2,2} , 4}};
+    }
+
+    @Test(dataProvider = "matrixProvider")
+    public void twoDimensionArrayTest(int[][] array,int N, int M, int[] check)
+    {
+        assertEquals(new Lab_0_Variant26().matrix_array(array,N,M), check);
+    }
+
+    @DataProvider
+    public Object[][] matrixProvider()
+    {
+        int[][] array = {{1, 2, 3},
+                {6, 2, 5},
+                {4, 2, 7}
+                };
+        int[] check = {8,2};
+
+        return new Object[][] {{array,3,3,check} };
+
     }
 
 }
